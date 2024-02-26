0ea4ac30cbe98463dc09f5cfdb2ecfec2d6962fc
Natalia García <ngarcia.bustamante@outlook.com>
2024-02-25 21:28:52 -0300

    Se añade smooth scroll a nav-items

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,16 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function(){
+    $(".nav-link").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 'normal', function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
