b1b06f8c0b46247cbd885ebb67b22774c5d34941
Natalia García <ngarcia.bustamante@outlook.com>
2024-02-26 00:14:56 -0300

    Se añaden comentarios en js y css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]') /*Función Tooltips*/
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $(document).ready(function(){
-    $(".nav-link").on('click', function(event) {
+    $(".nav-link").on('click', function(event) { /*Efecto smooth scroll*/
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
@@ -12,7 +12,7 @@ $(document).ready(function(){
                 window.location.hash = hash;
             });}
     });
-    $("#enviarcorreo").click(function(){
+    $("#enviarcorreo").click(function(){ /*Función alert*/
         alert("¡Muchas gracias! Hemos recibido tu correo.")
     })
 });
